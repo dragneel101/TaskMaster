70050e7d1888d83e58b0b3758a3b085e11129372
Raish khaitu <71054834+dragneel101@users.noreply.github.com>
2025-03-25 18:10:01 -0400

    Moved Firebase service account key into functions/ for deployment compatibility

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,9 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase/taskmaster-2a195-firebase-adminsdk-fbsvc-f4f4130e97.json");
+const serviceAccount = require(
+    "./taskmaster-service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-module.exports = { db };
+module.exports = {db};
