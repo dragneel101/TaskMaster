642c445b8915db7c47be5330e32deb162f2f00a0
Raish khaitu <71054834+dragneel101@users.noreply.github.com>
2025-03-25 10:35:25 -0400

    Set up Firebase Functions API with Express and GET /tasks endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./firebase");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const app = express();
+app.use(cors({ origin: true }));
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// GET /tasks - fetch tasks from Firestore
+app.get("/tasks", async (req, res) => {
+  try {
+    const snapshot = await db.collection("tasks").get();
+    const tasks = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.api = functions.https.onRequest(app);
