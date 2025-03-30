8db20505a0fa09b471700d0e505b7688e09ded63
Raish Khaitu <raishkhaitu@gmail.com>
2025-03-30 17:20:06 -0400

    Add UndoButton

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TaskForm from "../components/TaskForm";
 import TaskFilter from "../components/TaskFilter";
 import TaskList from "../components/TaskList";
+import UndoButton from "../components/UndoButton";
 
 
 /**
@@ -13,6 +14,7 @@ const Dashboard = () => {
     <div className="p-6 max-w-3xl mx-auto">
       <h1 className="text-2xl font-bold mb-6"> TaskMaster Dashboard</h1>
       <TaskForm />
+      <UndoButton />
       <TaskList />
       <TaskFilter />
     </div>
